f6d663604addc97c5297a9cfe9582da96dfc5290
Isaac Wilcove <isaac@wilcove.com>
2018-09-01 13:45:47 -0700

    Add lift and revert drivetrain

--- src/main/java/frc/team7308/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team7308/robot/subsystems/Drivetrain.java
@@ -1,62 +1,121 @@
-package frc.team7308.robot.subsystems;
+﻿package frc.team7308.robot.subsystems;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Spark;
 
 import frc.team7308.robot.ControlLoop;
+import frc.team7308.robot.DriverStation;
 import frc.team7308.robot.subsystems.Subsystem;
-import frc.team7308.robot.subsystems.SwerveModule;
-import frc.team7308.robot.util.Vector2D;
 
 public class Drivetrain extends Subsystem {
-    private SwerveModule topRight;
-    private SwerveModule topLeft;
-    private SwerveModule bottomRight;
-    private SwerveModule bottomLeft;
+    private SpeedControllerGroup left;
+    private SpeedControllerGroup right;
+    private double leftSpeed;
+    private double rightSpeed;
 
-    private double drivebaseWidth = 10.0;
-    private double drivebaseHeight = 10.0;
+    private static double mQuickStopAccumulator;
+    private static final double kStickDeadband = 0.02;
+    private static final double kWheelDeadband = 0.02;
+    private static final double kTurnSensitivity = 1.0;
 
-    private static SwerveConfig config;
+    public static final double kDefaultQuickStopThreshold = 0.2;
+    public static final double kDefaultQuickStopAlpha = 0.1;
+
+    private double m_quickStopThreshold = kDefaultQuickStopThreshold;
+    private double m_quickStopAlpha = kDefaultQuickStopAlpha;
+    private double m_quickStopAccumulator;
+    private double m_rightSideInvertMultiplier = -1.0;
 
     public final ControlLoop controlLoop = new ControlLoop() {
         @Override
         public void loopPeriodic() {
-            topRight.move();
-            topLeft.move();
-            bottomRight.move();
-            bottomLeft.move();
+            WheelDrive(DriverStation.driveThrottle, DriverStation.driveRotation, DriverStation.quickTurn);
+            left.set(leftSpeed);
+            right.set(rightSpeed);
         }
     };
 
     public Drivetrain() {
-        SwerveModule topRight = new SwerveModule(0, 4);
-        SwerveModule topLeft = new SwerveModule(1, 5);
-        SwerveModule bottomRight = new SwerveModule(2, 6);
-        SwerveModule bottomLeft = new SwerveModule(3, 7);
+        Spark frontLeft = new Spark(0);
+        Spark backLeft = new Spark(1);
+        Spark frontRight = new Spark(2);
+        Spark backRight = new Spark(3);
+
+        this.left = new SpeedControllerGroup(frontLeft, backLeft);
+        this.right = new SpeedControllerGroup(frontRight, backRight);
 
         controlLoop.start();
     }
 
-    public void SwerveDrive(double xMovement, double zMovement, double rotation) {
-        final double A = xMovement – rotation * drivebaseHeight / 2;
-        final double B = xMovement + rotation * drivebaseHeight / 2;
-        final double C = yMovement - rotation * drivebaseWidth / 2;
-        final double B = yMovement + rotation * drivebaseWidth / 2;
-    
-        topRight.setSpeed(Math.sqrt(B * B + D * D));
-        topRight.setAngle(Math.atan2(B, D));
+    public void ArcadeDrive(double movement, double rotation) {
+        this.leftSpeed = rotation + movement;
+        this.rightSpeed = rotation - movement;
+    }
 
-        topLeft.setSpeed(Math.sqrt(B * B + C * C));
-        topLeft.setAngle(Math.atan2(B, C));
+    public void TankDrive(double leftSpeed, double rightSpeed) {
+        this.leftSpeed = leftSpeed;
+        this.rightSpeed = rightSpeed;
+    }
 
-        bottomRight.setSpeed(Math.sqrt(A * A + C * C));
-        bottomRight.setAngle(Math.atan2(A, C));
+    public void WheelDrive(double throttle, double rotation, boolean quickTurn) {
+        double xSpeed = applyDeadzone(throttle, kStickDeadband);
+        double zRotation = applyDeadzone(rotation, kWheelDeadband);
 
-        bottomLeft.setSpeed(Math.sqrt(A * A + D * D));
-        bottomLeft.setAngle(Math.atan2(A, D));
+        double angularPower;
+        boolean overPower;
 
-        Vector2D movement = new Vector2D(xMovement, zMovement);
+        if (quickTurn) {
+            if (Math.abs(xSpeed) < m_quickStopThreshold) {
+                m_quickStopAccumulator = (1 - m_quickStopAlpha) * m_quickStopAccumulator
+                    + m_quickStopAlpha * zRotation * 2;
+            }
+            overPower = true;
+            angularPower = zRotation;
+        } else {
+            overPower = false;
+            angularPower = Math.abs(xSpeed) * zRotation - m_quickStopAccumulator;
 
+            if (m_quickStopAccumulator > 1) {
+                m_quickStopAccumulator -= 1;
+            } else if (m_quickStopAccumulator < -1) {
+                m_quickStopAccumulator += 1;
+            } else {
+                m_quickStopAccumulator = 0.0;
+            }
+        }
+
+        double leftMotorOutput = xSpeed + angularPower;
+        double rightMotorOutput = xSpeed - angularPower;
+
+        // If rotation is overpowered, reduce both outputs to within acceptable range
+        if (overPower) {
+            if (leftMotorOutput > 1.0) {
+                rightMotorOutput -= leftMotorOutput - 1.0;
+                leftMotorOutput = 1.0;
+            } else if (rightMotorOutput > 1.0) {
+                leftMotorOutput -= rightMotorOutput - 1.0;
+                rightMotorOutput = 1.0;
+            } else if (leftMotorOutput < -1.0) {
+                rightMotorOutput -= leftMotorOutput + 1.0;
+                leftMotorOutput = -1.0;
+            } else if (rightMotorOutput < -1.0) {
+                leftMotorOutput -= rightMotorOutput + 1.0;
+                rightMotorOutput = -1.0;
+            }
+        }
+
+        // Normalize the wheel speeds
+        double maxMagnitude = Math.max(Math.abs(leftMotorOutput), Math.abs(rightMotorOutput));
+        if (maxMagnitude > 1.0) {
+            leftMotorOutput /= maxMagnitude;
+            rightMotorOutput /= maxMagnitude;
+        }
+
+        leftSpeed = leftMotorOutput * 1.0;
+        rightSpeed = rightMotorOutput * 1.0 * m_rightSideInvertMultiplier;
+    }
+
+    public double applyDeadzone(double val, double deadband) {
+        return (Math.abs(val) > Math.abs(deadband)) ? val : 0.0;
     }
 }
